e356d03d67e31351567dc621f8e780d834203e06
Angellee <angelriapurnamasari17@gmail.com>
2022-02-27 21:04:42 +0800

    feat: roles constant

--- src/config/constant.js
+++ src/config/constant.js
@@ -4,4 +4,41 @@ exports.NODE_ENVIRONMENT = {
     prod: 'production'
 }
 
-
+exports.roles = {
+    GA: {
+        code: "GA",
+        name: "General Admin",
+    },
+    ADM: {
+        code: "ADM",
+        name: "Administrator",
+    },
+    OWN: {
+        code: "OWN",
+        name: "Owner"
+    },
+    OPS: { 
+        code: "OPS",
+        name: "Operational Team"
+    },
+    SPM: {
+        code: "SPM",
+        name: "Sales & Purchasing Manager"
+    },
+    SPV: {
+        code: "SPV",
+        name: "Supervisor"
+    },
+    SM: {
+        code: "SM",
+        name: "Sales & Marketing"
+    },
+    CS: {
+        code: "CS",
+        name: "Customer Service"
+    },
+    USR: {
+        code: "USR",
+        name: "User/Customer"
+    },
+}
