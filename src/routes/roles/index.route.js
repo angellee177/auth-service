d0e5bd74748d9322a47d1cb5467f63f880a3264f
Angellee <angelriapurnamasari17@gmail.com>
2022-03-15 00:31:26 +0800

    feat: roles routes

---
+++ src/routes/roles/index.route.js
@@ -0,0 +1,47 @@
+const express = require("express")
+    , routes = express.Router()
+    , { roles } = require('../../config/constant')
+    , RolesControllers = require('../../controllers/rolesController')
+    , AuthMiddleware = require('../../middleware/auth');
+
+routes.post(
+    "/new",
+    [
+        AuthMiddleware.authenticate([roles.GA['code'], roles.ADM['code'], roles.OWN['code']])
+    ],
+    RolesControllers.createNewRoles
+);
+
+routes.get(
+    "/",
+    [
+        AuthMiddleware.authenticate([roles.GA['code'], roles.ADM['code'], roles.OWN['code']])
+    ],
+    RolesControllers.roles,
+);
+
+routes.get(
+    "/:roleId",
+    [
+        AuthMiddleware.authenticate([roles.GA['code'], roles.OWN['code']])
+    ],
+    RolesControllers.role,
+);
+
+routes.put(
+    "/update/:roleId",
+    [
+        AuthMiddleware.authenticate([roles.GA['code'], roles.OWN['code']])
+    ],
+    RolesControllers.updatingRole,
+);
+
+routes.delete(
+    "/delete/:roleId",
+    [
+        AuthMiddleware.authenticate([roles.GA['code'], roles.OWN['code']])
+    ],
+    RolesControllers.deleteRole,
+);
+
+module.exports = routes;
