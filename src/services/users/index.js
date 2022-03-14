19f76a171c027f0d554e4964634fc048b85ab89d
Angellee <angelriapurnamasari17@gmail.com>
2022-03-15 00:33:21 +0800

    feat: users services

--- src/services/users/index.js
+++ src/services/users/index.js
@@ -1,17 +1,41 @@
 const User =  require('../../database/models/users/index')
     , { throwErrorsHttp, setLog } = require('../../helper/utils')
-    , { StatusCodes } = require('http-status-codes');
+    , { StatusCodes } = require('http-status-codes')
+    , Auth = require('../../middleware/auth')
+    , { roles: ROLE, branch_code: BRANCH } = require('../../config/constant')
+    , { get } = require('lodash')
+    , bcrypt = require('bcrypt');
 
 /**
  * 
  * Create new User
  * 
- * @param {String} username 
- * @param {String} email 
- * @param {String} password 
- * @param {String} roles
+ * @param {Object} payload
+ * @param {String} payload.username 
+ * @param {String} payload.email 
+ * @param {String} payload.password 
+ * @param {String} payload.roles
+ * @param {String} payload.fullname
+ * @param {String} payload.phoneNumber
+ * @param {String} payload.dob
+ * @param {String} payload.address
+ * @param {String} payload.nip employee identity number
+ * @param {String} payload.branchCode
  */
- const register = async(username, email, password, roles) => {
+ const register = async(payload) => {     
+    const { 
+        username, 
+        email,
+        password, 
+        roles = [ ROLE['USR']['code'] ],
+        fullname,
+        phoneNumber = null,
+        dob = null,
+        address = null,
+        nip = null, 
+        branchCode = BRANCH.MGL,
+    } = payload;
+    
     const userInfo = await User.findOne({ email });
       
     if(userInfo) {
@@ -26,13 +50,19 @@ const User =  require('../../database/models/users/index')
     const user = new User({
         username,
         email,
+        fullname,
         password,
-        roles, 
+        roles,
+        phoneNumber,
+        dob,
+        address,
+        nip,
+        branchCode,
     });
 
     await user.save();
 
-    return user;
+    return true;
 };
 
 /**
@@ -46,4 +76,82 @@ const users = async() => {
     return userList;
 };
 
-module.exports = { register, users };
+/**
+ * Generate Auth Token
+ * 
+ */
+ const generateAuthToken = async(email, password) => {
+
+        const userInfo = await User.findOne({ email });
+
+        if(!userInfo) {
+            const errorMessage = "Oops, wrong email. please kindly check again.";
+            setLog({
+                level: 'User Services', method: 'Login failed', message: errorMessage, others: email
+            });
+
+            throwErrorsHttp(errorMessage, StatusCodes.NOT_FOUND);
+        }
+        
+        const validPassword = await bcrypt.compare(password, userInfo['password']);
+
+        if(!validPassword) {
+            const errorMessage = "Oops, wrong password. please kindly check again.";
+            setLog({
+                level: 'User Services', method: 'Login failed', message: errorMessage, others: email
+            });
+
+            throwErrorsHttp(errorMessage, StatusCodes.NOT_FOUND);
+        };
+
+        // get user info
+        const role = get(userInfo, 'roles');
+
+        const token = await Auth.encodingToken(email, role);
+
+        setLog({
+            level: 'User Services', method: 'Generate Auth Token Success', message: email, others: token
+        });
+
+        return token;
+};
+
+/**
+ * 
+ * Update User Profile.
+ * 
+ * @param {Object} payload
+ * @param {String} payload.username 
+ * @param {String} payload.password 
+ * @param {String} payload.roles
+ * @param {String} payload.fullname
+ * @param {String} payload.phoneNumber
+ * @param {String} payload.dob
+ * @param {String} payload.address
+ * @param {String} payload.nip employee identity number
+ * @param {String} payload.branchCode
+ * @param {String} email 
+ */
+const update = async(payload, email) => {
+    const userInfo = await User.findOne({ email });
+      
+    if(userInfo) {
+        const errorMessage = "Oops, You already have account with this email!";
+        setLog({
+            level: 'Users Services', method: 'Register Failed', message: errorMessage
+        });
+
+        throwErrorsHttp(errorMessage, StatusCodes.NOT_FOUND);
+    };
+
+    const result = await User.update({ _id: userInfo._id }, payload);
+
+    return result;
+}
+
+module.exports = { 
+    register, 
+    users, 
+    generateAuthToken,
+    update, 
+};
--- src/services/users/index.test.js
+++ src/services/users/index.test.js
@@ -1,13 +1,20 @@
 const User = require('../../database/models/users');
 const Service = require('./index');
-const { StatusCodes } = require('http-status-codes'); 
+const { StatusCodes } = require('http-status-codes');
+const { roles: ROLE } = require('../../config/constant'); 
+const Auth = require('../../middleware/auth');
+const bcrypt = require('bcrypt');
 
- jest.mock('../../database/models/users');
-
- describe("Users Services Test Suite", () => {
+jest.mock('bcrypt');
+jest.mock('../../database/models/users');
+describe("Users Services Test Suite", () => {
     beforeEach(() => { 
-        User.mockClear();
+        jest.clearAllMocks();
     });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    })
 
     describe("register function", () => {
         const { register } = Service;
@@ -16,20 +23,53 @@ const { StatusCodes } = require('http-status-codes');
             expect(register).toBeDefined();
         });
 
-        // it("success response", async() => {
-        //     const user = fakeUser[0];
-        //     const spy = spyUserFindOne(null);
+        it("success response", async() => {
+            const fakeUsers = getUsers();
 
-        //     const mockUserSave = jest.fn();
-        //     const 
-        // });
+            const mockUserSave = jest.fn()
+                .mockResolvedValueOnce(true);
+            User.save = mockUserSave;
+
+            const spy = spyUserFindOne(null);
+            
+            const result = await Service.register(fakeUsers[0]);
+
+            expect(spy).toHaveBeenCalledTimes(1);
+            expect(spy.mock.calls[0][0]).toMatchObject({ email: fakeUsers[0]['email'] });
+            
+            expect(result).toBeTruthy();
+        });
+
+        it("success response with default value", async() => {
+            const fakeUsers = {
+                username: "fake-username",
+                email: "fake-meail",
+                password: "fake-password",
+                fullname: "fake-fullname",
+            };
+
+            const mockUserSave = jest.fn()
+                .mockResolvedValueOnce(true);
+            User.save = mockUserSave;
+
+            const spy = spyUserFindOne(null);
+            
+            const result = await Service.register(fakeUsers);
+
+            expect(spy).toHaveBeenCalledTimes(1);
+            expect(spy.mock.calls[0][0]).toMatchObject({ email: fakeUsers['email'] });
+            
+            expect(result).toBeTruthy();
+        });
 
         it("throw default error", async() => {
-            const user = fakeUsers[0];
-            const spy = spyUserFindOne(fakeUsers[0]);
+            const users = getUsers();
+            const spy = spyUserFindOne(users[0]);
 
             try {
-                await register(user['username'], user['email'], user['password']);
+                await register(
+                    users[0]
+                );
             } catch(e) {                
                 expect(e).toBeDefined();
                 expect(e.message).toBe('Oops, You already have account with this email!');
@@ -37,7 +77,7 @@ const { StatusCodes } = require('http-status-codes');
 
                 expect(spy).toHaveBeenCalledTimes(1);
                 expect(spy.mock.calls[0][0]).toBeDefined();
-                expect(spy.mock.calls[0][0]).toMatchObject({ email: user['email'] });
+                expect(spy.mock.calls[0][0]).toMatchObject({ email: users[0]['email'] });
                 
                 expect(spy.mock.calls[0][1]).toBeUndefined();
             }
@@ -52,6 +92,8 @@ const { StatusCodes } = require('http-status-codes');
         });
 
         it("success response", async() => {
+            const fakeUsers = getUsers();
+
             const spy = spyUserFind(fakeUsers);
 
             const result = await users();
@@ -63,6 +105,103 @@ const { StatusCodes } = require('http-status-codes');
             expect(result).toMatchObject(fakeUsers);
         });
     });
+
+    describe("generateAuthToken function", () => {
+        const { generateAuthToken } = Service;
+
+        it("should be defined", () => {
+            expect(generateAuthToken).toBeDefined();
+        });
+
+        it("success response", async() => {
+            const fakeUsers = getUsers();
+            const spyUser = spyUserFindOne(fakeUsers[0]);
+
+            const fakeVal = true;
+            const mockBcryptCompare = jest.fn(() => ({ 
+                compare: jest.fn(() => fakeVal)
+            }));
+        
+            const mockBcrypt = jest.spyOn(bcrypt, 'compare')
+                .mockImplementation(mockBcryptCompare);
+
+            const fakeToken = "fake-token";
+            const spyGenerateToken = spyEncodingToken(fakeToken);
+
+            const result = await generateAuthToken(fakeUsers[0]['email'], fakeUsers[0]['password']);
+
+            expect(spyUser).toHaveBeenCalledTimes(1);
+            expect(spyUser.mock.calls[0][0]).toMatchObject({ email: fakeUsers[0]['email'] });
+
+            expect(mockBcrypt).toHaveBeenCalledTimes(1); 
+
+            expect(mockBcryptCompare).toHaveBeenCalledTimes(1);
+            expect(mockBcryptCompare.mock.calls[0][0]).toBe(fakeUsers[0]['password']);
+            expect(mockBcryptCompare.mock.calls[0][1]).toBe(fakeUsers[0]['password']);
+
+            expect(spyGenerateToken).toHaveBeenCalledTimes(1);
+            expect(spyGenerateToken.mock.calls[0][0]).toBe(fakeUsers[0]['email']);
+            expect(spyGenerateToken.mock.calls[0][1]).toBe(fakeUsers[0]['roles']);
+
+            expect(result).toBe(fakeToken);
+        });
+
+        it("throw default error if wrong email", async() => {
+            const fakeUsers = getUsers();
+            const spyUser = spyUserFindOne(null);
+
+            const fakeVal = false;
+            const mockBcrypt = spyBcryptCompare(fakeVal);
+
+            try {
+                await generateAuthToken(fakeUsers[0]['email'], fakeUsers[0]['password']);
+            } catch(e) {      
+                expect(e).toBeDefined();
+                expect(e.message).toBe('Oops, wrong email. please kindly check again.');
+                expect(e.httpStatusCode).toBe(StatusCodes.NOT_FOUND);
+
+                expect(spyUser).toHaveBeenCalledTimes(1);
+                expect(spyUser.mock.calls[0][0]).toBeDefined();
+                expect(spyUser.mock.calls[0][0]).toMatchObject({ email: fakeUsers[0]['email'] });
+                expect(spyUser.mock.calls[0][1]).toBeUndefined();
+
+                expect(mockBcrypt).toHaveBeenCalledTimes(0);
+            }
+        });
+
+        it("throw default error if wrong password", async() => {
+            const fakeUsers = getUsers();
+            const spyUser = spyUserFindOne(fakeUsers[0]['email']);
+
+            const fakeVal = false;
+            const mockBcrypt = spyBcryptCompare(fakeVal);
+
+            try {
+                await generateAuthToken(fakeUsers[0]['email'], fakeUsers[0]['password']);
+            } catch(e) {                                
+                expect(e).toBeDefined();
+                expect(e.message).toBe('Oops, wrong password. please kindly check again.');
+                expect(e.httpStatusCode).toBe(StatusCodes.NOT_FOUND);
+
+                expect(spyUser).toHaveBeenCalledTimes(1);
+                expect(spyUser.mock.calls[0][0]).toBeDefined();
+                expect(spyUser.mock.calls[0][0]).toMatchObject({ email: fakeUsers[0]['email'] });
+                expect(spyUser.mock.calls[0][1]).toBeUndefined();
+
+                expect(mockBcrypt).toHaveBeenCalledTimes(1);
+            }
+        });
+    });
+
+    describe("update function", () => {
+        const { update } = Service;
+
+        it("should be defined", async() => {
+            expect(update).toBeDefined();
+        });
+
+        
+    });
  });
 
 const spyUserFindOne = (result, isResolve = true) => (isResolve 
@@ -73,12 +212,20 @@ const spyUserFind = (result, isResolve = true) => (isResolve
     ? jest.spyOn(User, 'find').mockResolvedValue(result)
     : jest.spyOn(User, 'find').mockRejectedValue(result));
 
-const fakeUsers = [{
+const spyEncodingToken = (result, isResolve = true) => (isResolve
+    ? jest.spyOn(Auth, 'encodingToken').mockResolvedValue(result)
+    : jest.spyOn(Auth, 'encodingToken').mockRejectedValue(result));
+
+const spyBcryptCompare = (result, isResolve = true) => (isResolve
+    ? jest.spyOn(bcrypt, 'compare').mockResolvedValue(result)
+    : jest.spyOn(bcrypt, 'compare').mockRejectedValue(result));
+
+const getUsers = () => [{
     username: 'fakeUsername',
     email: 'fake@mail.com',
     password: 'fakePassword',
-    role: 1,
-    full_name: "fakeUser",
-    contact_number: "085712345678",
+    roles: [ ROLE['USR']['code']],
+    fullame: "fakeUser",
+    phoneNumber: "085712345678",
     address: 'fake street, fake number'
 }]
