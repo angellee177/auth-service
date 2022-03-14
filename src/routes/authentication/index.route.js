1b9f2d973030a834eeded5e711e2a4db32a442b8
Angellee <angelriapurnamasari17@gmail.com>
2022-03-15 00:31:58 +0800

    feat: authentication routes

--- src/routes/authentication/index.route.js
+++ src/routes/authentication/index.route.js
@@ -3,7 +3,6 @@ const express = require('express')
     , AuthControllers = require('../../controllers/authController')
 
 routes.post('/login', AuthControllers.login);
-routes.post('/generate-token', AuthControllers.generateAuthToken);
 routes.post('/register', AuthControllers.register);
 
 module.exports = routes;
