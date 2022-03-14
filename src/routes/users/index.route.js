38ba051835ef211003d38daaddd123f8560da72b
Angellee <angelriapurnamasari17@gmail.com>
2022-03-15 00:32:21 +0800

    feat:  users routes

--- src/routes/users/index.route.js
+++ src/routes/users/index.route.js
@@ -1,15 +1,33 @@
 const express = require('express')
     , routes = express.Router()
     , { roles } = require('../../config/constant')
-    , AuthControllers = require('../../controllers/authController')
+    , Controllers = require('../../controllers/userController')
     , AuthMiddleware = require('../../middleware/auth');
 
 routes.get(
     '/', 
     [
-        AuthMiddleware.authenticate([roles.ADM, roles.GA])
+        AuthMiddleware.authenticate([roles['ADM']['code'], roles['GA']['code']])
     ],
-    AuthControllers.users
+    Controllers.users,
+)
+
+routes.put(
+    '/update/my-profile',
+    [
+        AuthMiddleware.authenticate([
+            roles['ADM']['code'],
+            roles['GA']['code'],
+            roles['CS']['code'],
+            roles['OPS']['code'],
+            roles['OWN']['code'],
+            roles['SM']['code'],
+            roles['SPM']['code'],
+            roles['SPV']['code'],
+            roles['USR']['code'],
+        ]),
+    ],
+    Controllers.update,
 )
 
 module.exports = routes;
