a59fecfe82ea8b5dc057aae55a6b59f69c45c8a3
Angellee <angelriapurnamasari17@gmail.com>
2022-03-23 23:31:45 +0800

    feat: UserRoutes

--- src/routes/users/index.route.js
+++ src/routes/users/index.route.js
@@ -4,10 +4,24 @@ const express = require('express')
     , Controllers = require('../../controllers/userController')
     , AuthMiddleware = require('../../middleware/auth');
 
+routes.post(
+    '/new-account',
+    [
+        AuthMiddleware.authenticate([roles['OWN']]),
+    ],
+    Controllers.newUser
+);
+
+
 routes.get(
     '/', 
     [
-        AuthMiddleware.authenticate([roles['ADM']['code'], roles['GA']['code']])
+        AuthMiddleware.authenticate([
+            roles['ADM']['code'], 
+            roles['GA']['code'],
+            roles['OWN']['code'],
+        ]),
+        
     ],
     Controllers.users,
 )
@@ -28,6 +42,24 @@ routes.get(
         ]),
     ],
     Controllers.user,
+)
+
+routes.get(
+    '/my-profile',
+    [
+        AuthMiddleware.authenticate([
+            roles['ADM']['code'],
+            roles['GA']['code'],
+            roles['CS']['code'],
+            roles['OPS']['code'],
+            roles['OWN']['code'],
+            roles['SM']['code'],
+            roles['SPM']['code'],
+            roles['SPV']['code'],
+            roles['USR']['code'],
+        ]),
+    ],
+    Controllers.myProfile,
 )
 
 routes.put(
