1f8511ae0478259b68fde72b9fe6fa023143e94e
Angellee <angelriapurnamasari17@gmail.com>
2022-03-23 23:32:43 +0800

    add: new-user routes

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -1,9 +1,11 @@
 const express       = require('express')
     , routes        = express.Router()
     , userRoutes   = require('./users/index.route')
-    , authRoutes    = require('./authentication/index.route');
+    , authRoutes    = require('./authentication/index.route')
+    , rolesRoutes = require('./roles/index.route');
 
 routes.use('/auth'  ,   authRoutes);
 routes.use('/users' ,   userRoutes);
+routes.use('/roles',    rolesRoutes);
 
 module.exports = routes;
